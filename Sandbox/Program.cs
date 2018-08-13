66102ec1d791882e560d2809834b75c025091079
nhancock31 <41527777+nhancock31@users.noreply.github.com>
2018-08-12 19:39:03 -0500

    Fibonacci (#1)
    * remove c
    
    * WriteFib.1
    
    * writefib.2+readfib.1
    
    * 1st task

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
+using System.IO;
 
 namespace Sandbox
 {
-    class Program 
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World" + "\n" + "Press any key to continue.");
-            Console.ReadKey();
-              //nube
+            WriteFibonacci();
+           
+
         }
+        static void WriteFibonacci()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\fibonacci.txt";
+            double next = 0;
+            double first = 0;
+            double second = 1;
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                    for (int y = 1; y < 101; y++)
+                    {
+                        next = first + second;
+                        int z = (y % 10);
+                        bool ck = (z == 0) ? true : false;
+                        if (ck)
+                        {
+                            sw.WriteLine(next + ",");
+                        }
+                        else
+                        {
+                            sw.Write(next + ",");
+                        }
+                        first = second;
+                        second = next;
+                    }
+            }
+        }
+       
     }
 }
