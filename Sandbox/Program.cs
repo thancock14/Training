ff1be4492bee89a758edda8f5ffceeb824d86c69
NHancock31 <NHancock31@gmail.com>
2018-08-19 13:30:25 -0500

    Created an initial "option screen" loop.  I also adjusted a few ending codes for select methods in order to fit more with loop.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -8,9 +8,51 @@ namespace Sandbox
     {
         public static void Main(string[] args)
         {
-            WriteFibonacci();
-            ReadFibonacci();
-            ReadToFile();
+
+
+            Console.WriteLine("Welcome to Fibonacci for beginners. IN C#!!!");
+
+            Begin:
+            Console.WriteLine("\nPlease select an available option, then press Enter.");
+            Console.WriteLine("   0 >>> Write first 100 fibonacci numbers to text file on Desktop.");
+            Console.WriteLine("   1 >>> Read the first 100 fibonacci numbers to the current window.");
+            Console.WriteLine("   2 >>> Write the first 100 fibonacci numbers to a Text file of your choice on Desktop.");
+            Console.WriteLine("\n   9 >>> Exit window.");
+            string read = Console.ReadLine();
+            try
+            {
+                int option = int.Parse(read);
+
+                switch (option)
+                {
+                    case 0:
+                        WriteFibonacci();
+                        break;
+                    case 1:
+                        ReadFibonacci();
+                        break;
+                    case 2:
+                        ReadToFile();
+                        break;
+                    case 9:
+                        goto Finish;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose a valid option.");
+                        break;
+                }
+                goto Begin;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                Console.WriteLine("Please choose a valid option.");
+                goto Begin;
+            }
+
+            Finish:;
+            
+
 
         }
 
@@ -41,12 +83,16 @@ namespace Sandbox
                         }
                     }
             }
+
+            Console.WriteLine("\nIt is finished.\n\nPress any key to go back to options.");
+            Console.ReadKey();
         }
 
         /*This function writes fibonacci numbers from fibonacci.txt file to console 
         1 line at a time waiting for user to press enter for next line*/
         static void ReadFibonacci()
         {
+            Startread:
             String line;
             ConsoleKeyInfo k;
 
@@ -71,19 +117,24 @@ namespace Sandbox
                       line = sr.ReadLine();
                 }
 
-                Console.WriteLine("You have reached the end of the file. Press any key to exit.");
+                Console.WriteLine("You have reached the end of the file. Press any key to continue.");
                 Console.ReadKey();
                 sr.Close();
                 
             }
             catch (Exception e)
             {
-                Console.WriteLine("Exception: " + e.Message);
+                WriteFibonacci();
+                goto Startread;
+                //Console.WriteLine("Exception: " + e.Message);
             }
             finally
             {
-                Console.WriteLine("Executing finally block.");
+                Console.WriteLine();
             }
+
+            Console.WriteLine("\n!!!!!Wow this code is good!!!!!\nPress any key to go back to options.");
+            Console.ReadKey();
         }
 
         //This function writes the fibonacci numbers to a file of the user's choice.
@@ -116,12 +167,23 @@ namespace Sandbox
             } while (!File.Exists(fullpath));
 
             //fullpath = path + "//Test.txt";
-            string Fibpath = path + "\\fibonacci.txt";
-            var Linestoread = from line in File.ReadLines(Fibpath)
-                               select line;
+            Writestart:
+            try
+            {
+                string Fibpath = path + "\\fibonacci.txt";
+                var Linestoread = from line in File.ReadLines(Fibpath)
+                                  select line;
 
-            File.AppendAllLines(fullpath, Linestoread);
-            
+                File.AppendAllLines(fullpath, Linestoread);
+
+                Console.WriteLine("\nThat was impressive!\nWhat will he do next!!!\n\nPress any key to go back to options.");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                WriteFibonacci();
+                goto Writestart;
+            }
         }
     }
 }
